3e5740dea37b478d449f2b38f59fce48052df79c
SavageBlink <driss@LAPTOP-FI9518O6.localdomain>
2020-12-30 17:56:50 +0100

    je suis fatigué de ce projet

--- src/headers/engine.h
+++ src/headers/engine.h
@@ -1,9 +1,45 @@
+/**
+ * @file engine.h
+ * @author Guillaume/Hendercraft Driss/SavageBlink
+ * @date 30 december 2020
+ * @brief Header file of the main inferrence engine, it also defines a linked-list of facts (true propo
+ * sitions)
+ */
+
 #ifndef ENGINE
 #define ENGINE
-#include <stdlib.h>
-#include <stdio.h>
+#include "knowledgeBase.h"
+
+typedef struct factElem{
+  Proposition* value;
+  struct factElem * next;
+}factElement
+
+typedef factElement * FB;
 
 
+/**
+ * @param NONE
+ * @return Return a pointer to an empty base of facts
+ * @brief Create an empty linked-list of facts
+ */
+FB* createEmptyFB();
 
+/**
+ * @param fb-Pointer to a base of facts you want to modify
+ * @param P-Pointer to a proposition you want to add 
+ * @return Return a modified base of facts
+ * @brief Add facts to a List of facts
+ */
+
+FB* addFact(FB* fb,Proposition* P);
+
+/**
+ * @param fb-pointer to a fact base
+ * @param id-string which identify the fact you want to delete
+ * @Return Return a modified base of facts
+ * @brief Delete a fact from a fact base
+ */
+FB* deleteFact(FB* fb, char * id);
 
 #endif
