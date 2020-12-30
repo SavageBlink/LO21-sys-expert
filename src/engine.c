794c22eb36cabbee62cfa470ab44f3c1326e0a7b
SavageBlink <driss@LAPTOP-FI9518O6.localdomain>
2020-12-30 22:10:54 +0100

    commit de la mort qui tue le RuNEnGiNe

--- src/engine.c
+++ src/engine.c
@@ -20,8 +20,44 @@ void printPtrue(FB* fb){
     }
     return;
 }
-    
 
+boolean computeRule(Rule* R)
+{
+  if(isEmpty(R))
+    {
+      fprintf(stderr,"Error while computing Rule (R : %p)",(void*) R);
+      return NULL;
+    }
+  else if(R->Conclusion == NULL)
+    {
+      fprintf(stderr, "Error while computing Rule (R->Conclusion : %p)",(void*) R->Conclusion);
+      return NULL;
+    }
+
+  if(R->Conclusion->value == true)
+    {
+      return false;
+    }
+  else if(isEmptyPre(R))
+    {
+      return true;
+    }
+  
+}
+
+FB* RuNEnGiNe(KB* kb, FB* fb)
+{
+  if(kb == NULL || fb == NULL)
+    {
+      fprintf(stderr,"Error while computing pointers might be NULL in RuNEnGiNe (kb : %p, fb %p)\n",(void*) kb,(void*)fb);
+      return NULL;
+    }
+
+  KBElement * pkb = *kb; /*Pointer on kb*/
+  factElement * pfb = *fb; /*Pointer on fb*/
+  
+  
+}
     
 
 
